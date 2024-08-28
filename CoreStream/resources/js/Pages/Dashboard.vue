3e5ad90ffacdf6c1c61d1d74edc35a2b4060f0eb
Pablo <PabloHAlves99@gmail.com>
2024-08-28 03:22:30 -0300

    Up position Head

--- CoreStream/resources/js/Pages/Dashboard.vue
+++ CoreStream/resources/js/Pages/Dashboard.vue
@@ -4,9 +4,9 @@ import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <Head title="Dashboard" />
 
     <AuthenticatedLayout>
+        <Head title="Dashboard" />
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
         </template>
