4a1689b2559d5087e605f5bc22f2c23c2dc41d6f
Pablo <PabloHAlves99@gmail.com>
2024-09-04 01:26:31 -0300

    Update Users.vue

--- CoreStream/resources/js/Pages/Users.vue
+++ CoreStream/resources/js/Pages/Users.vue
@@ -1,6 +1,63 @@
-<script setup>
-import { Head } from '@inertiajs/vue3';
+<template>
+    <div>
+        <!-- Container -->
+        <div class="container-fixed">
+        <div class="flex flex-wrap items-center lg:items-end justify-between gap-5 pb-7.5">
+        <div class="flex flex-col justify-center gap-2">
+        <h1 class="text-xl font-semibold leading-none text-gray-900">
+            Registered Users
+        </h1>
+        <div class="flex items-center gap-2 text-sm font-normal text-gray-700">
+            List of users registered in the database with main contact information
+        </div>
+        </div>
+        <div class="flex items-center gap-2.5">
+        <a class="btn btn-sm btn-light" href="#">
+            View Profile
+        </a>
+        </div>
+        </div>
+        </div>
+        <!-- End of Container -->
+        <!-- Container: list of users-->
+        <div class="container-fixed list-users">
 
+            <h1 class="title-list-users">User List</h1>
+            <div class="grid gap-5 lg:gap-7.5">
+
+                <div class="container-user-table-list-users">
+                    <table class="user-table-list-users">
+                        <thead class = "thead-list-users">
+                            <tr>
+                                <th class = "th-list-users">Login</th>
+                                <th class = "th-list-users">Name</th>
+                                <th class = "th-list-users">Phone</th>
+                                <th class = "th-list-users">Profile</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="user in users.data" :key="user.id">
+                                <td>{{ user.login }}</td>
+                                <td>{{ user.name }}</td>
+                                <td>{{ user.phone }}</td>
+                                <td>{{ user.profile }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+            <div class="pagination-list-users">
+                <button @click="previousPage" :disabled="!users.prev_page_url" class="pagination-list-users-button">Anterior</button>
+                <button @click="nextPage" :disabled="!users.next_page_url" class="pagination-list-users-button">Próximo</button>
+                <span class="pagination-list-users-info">Página {{ users.current_page }} de {{ users.last_page }}</span>
+            </div>
+
+        </div>
+    </div>
+    </div>
+</template>
+
+<script setup>
 const props = defineProps({
     users: Object,
 });
@@ -18,96 +75,47 @@ const previousPage = () => {
 };
 </script>
 
-<template>
-    <div class="container-all">
-        <Head title="Registered Users"/>
-
-        <h1 class="title">Lista de Usuários</h1>
-        <div class="container">
-
-            <table class="user-table">
-                <thead>
-                    <tr>
-                        <th>Nome</th>
-                        <th>Email</th>
-                        <th>Telefone</th>
-                        <th>Perfil</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="user in users.data" :key="user.id">
-                        <td>{{ user.name }}</td>
-                        <td>{{ user.email }}</td>
-                        <td>{{ user.phone }}</td>
-                        <td>{{ user.profile }}</td>
-                    </tr>
-                </tbody>
-            </table>
-
-            <div class="pagination">
-                <button @click="previousPage" :disabled="!users.prev_page_url" class="pagination-button">Anterior</button>
-                <button @click="nextPage" :disabled="!users.next_page_url" class="pagination-button">Próximo</button>
-                <span class="pagination-info">Página {{ users.current_page }} de {{ users.last_page }}</span>
-            </div>
-
-        </div>
-    </div>
-</template>
-
 <style scoped>
-.container-all {
-    background-color: #171923;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    padding: 15px;
+.list-users, .user-page-title{
+    text-align: center;
 }
-
-.container {
-    flex: 1;
-    max-width: 800px;
-    margin: 0 auto;
-    padding: 20px 30px;
-    background-color: #fff;
-    border-radius: 8px;
+.title-list-users {
+    font-size: 2rem;
+    margin-bottom: 15px;
+    font-weight: bold;
+}
+.container-user-table-list-users{
+    background-color: #FEFEFE;
+    color: #0D0E12;
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 }
 
-.title {
-    font-size: 2.5rem;
-    margin-bottom: 15px;
-    text-align: center;
-    color: #F9322C;
-    font-weight: bold;
-}
-
-.user-table {
+.user-table-list-users {
     width: 100%;
     border-collapse: collapse;
     margin-bottom: 20px;
 }
 
-.user-table th, .user-table td {
+.user-table-list-users th, .user-table-list-users td {
     padding: 12px;
-    text-align: left;
+    text-align: center;
     border-bottom: 1px solid #ddd;
 }
 
-.user-table th {
+.user-table-list-users th {
     background-color: #f4f4f4;
-    font-weight: bold;
 }
 
-.pagination {
+.pagination-list-users {
     display: flex;
     justify-content: center;
     align-items: center;
     gap: 10px;
 }
 
-.pagination-button {
-    background-color: #007bff;
-    color: white;
+.pagination-list-users-button {
+    background-color: #A3C9D9;
+    color: #0D0E12;
     border: none;
     border-radius: 5px;
     padding: 10px 20px;
@@ -116,16 +124,17 @@ const previousPage = () => {
     transition: background-color 0.3s;
 }
 
-.pagination-button:hover:not(:disabled) {
-    background-color: #0056b3;
+.pagination-list-users-button:hover:not(:disabled) {
+    background-color: #A3C9D9;
 }
 
-.pagination-button:disabled {
-    background-color: #ccc;
+.pagination-list-users-button:disabled {
+    background-color: #ddd;
     cursor: not-allowed;
 }
 
-.pagination-info {
+.pagination-list-users-info {
     font-size: 1rem;
 }
+
 </style>
