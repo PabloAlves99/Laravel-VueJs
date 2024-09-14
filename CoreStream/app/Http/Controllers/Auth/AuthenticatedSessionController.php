8a1bb1a6a138392239a5ae7057b2634165cdf355
Pablo <PabloHAlves99@gmail.com>
2024-09-14 15:03:45 -0300

    Update AuthenticatedSessionController.php

--- CoreStream/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ CoreStream/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -8,16 +8,17 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use Illuminate\View\View;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class AuthenticatedSessionController extends Controller
 {
     /**
      * Display the login view.
      */
-    public function create(): View
+    public function create(): Response
     {
-        return view('auth.login', [
+        return Inertia::render('Auth/Login', [
             'canResetPassword' => Route::has('password.request'),
             'status' => session('status'),
         ]);
