7d29c1d8c0b76262f5d69c037c9e8c5707b00a15
Pablo <PabloHAlves99@gmail.com>
2024-09-14 23:35:02 -0300

    Up in page login

---
+++ CoreStream/resources/js/Components/AlterTheme.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button
+      class="btn btn-icon btn-light dark:hidden"
+      data-theme-toggle="true"
+      data-tooltip="#theme_mode_dark"
+    >
+      <i class="ki-outline ki-sun"> </i>
+    </button>
+    <button
+      class="btn btn-icon btn-light hidden dark:flex"
+      data-theme-toggle="true"
+      data-tooltip="#theme_mode_light"
+    >
+      <i class="ki-outline ki-moon"> </i>
+    </button>
+    <div class="tooltip" id="theme_mode_light">Switch to Light mode</div>
+    <div class="tooltip" id="theme_mode_dark">Switch to Dark mode</div>
+  </div>
+</template>
--- CoreStream/resources/js/Pages/Auth/Login.vue
+++ CoreStream/resources/js/Pages/Auth/Login.vue
@@ -1,11 +1,12 @@
 <script setup>
-import Checkbox from '@/Components/Checkbox.vue';
-import GuestLayout from '@/Layouts/GuestLayout.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
-import { Head, Link, useForm } from '@inertiajs/vue3';
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import AlterTheme from "@/Components/AlterTheme.vue";
+import Checkbox from "@/Components/Checkbox.vue";
+import InputError from "@/Components/InputError.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import TextInput from "@/Components/TextInput.vue";
+import { Head, Link, useForm } from "@inertiajs/vue3";
 
 defineProps({
     canResetPassword: {
@@ -17,84 +18,138 @@ defineProps({
 });
 
 const form = useForm({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
     remember: false,
 });
 
 const submit = () => {
-    form.post(route('login'), {
-        onFinish: () => form.reset('password'),
+    form.post(route("login"), {
+        onFinish: () => form.reset("password"),
     });
 };
 </script>
 
 <template>
-    <GuestLayout>
+    <div class="container-fixed mg-tp">
+
         <Head title="Log in" />
+        <AlterTheme class="alter-theme" />
+        <div class="form-image-wrapper">
+            <ApplicationLogo class="app-logo" />
+            <div class="container-form-register">
+                <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+                    {{ status }}
+                </div>
 
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
+                <form @submit.prevent="submit">
+                    <div class="w-full pa-t5">
+                        <div class="items-baseline flex-wrap lg:flex-nowrap gap-2.5">
+                            <InputLabel class="form-label max-w-32" for="email" value="Email" />
+
+                            <TextInput id="email" type="email" class="input marg-5" v-model="form.email" required
+                                autocomplete="username" />
+                        </div>
+                    </div>
+                    <InputError class="form-hint input-center" :message="form.errors.email" />
+
+                    <div class="w-full pa-t5">
+                        <div class="items-baseline flex-wrap lg:flex-nowrap gap-2.5 pad-10">
+                            <InputLabel class="form-label max-w-32" for="password" value="Password" />
+
+                            <TextInput id="password" type="password" class="input marg-5" v-model="form.password"
+                                required autocomplete="new-password" />
+                        </div>
+                    </div>
+                    <InputError class="form-hint input-center" :message="form.errors.password" />
+
+                    <div class="block mt-4">
+                        <label class="flex items-center">
+                            <Checkbox name="remember" v-model:checked="form.remember" />
+                            <span class="ms-2 text-sm text-gray-600">Remember me</span>
+                        </label>
+                    </div>
+
+                    <div class="flex items-center justify-end mt-4">
+                        <Link v-if="canResetPassword" :href="route('password.request')"
+                            class="underline text-sm text-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        Forgot your password?
+                        </Link>
+
+                        <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }"
+                            :disabled="form.processing">
+                            Log in
+                        </PrimaryButton>
+                    </div>
+                </form>
+            </div>
         </div>
-
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
-
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
-
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
-
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
-
-                <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="current-password"
-                />
-
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
-
-            <div class="block mt-4">
-                <label class="flex items-center">
-                    <Checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ms-2 text-sm text-gray-600">Remember me</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <Link
-                    v-if="canResetPassword"
-                    :href="route('password.request')"
-                    class="underline text-sm text-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    Forgot your password?
-                </Link>
-
-                <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Log in
-                </PrimaryButton>
-            </div>
-        </form>
-    </GuestLayout>
+    </div>
 </template>
 
 <style>
-span{
-    color: #071437dc;
+.mg-tp {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+
+.form-image-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.app-logo {
+    max-width: 250px;
+}
+
+.container-form-register {
+    width: 400px;
+}
+
+.pad-10 {
+    padding-top: 10px;
+}
+
+.marg-5 {
+    margin-top: 5px;
+}
+
+.alter-theme {
+    position: absolute;
+    top: 1%;
+    right: 1%;
+    display: flex;
+    align-items: center;
+}
+
+@media (max-width: 800px) {
+    .form-image-wrapper {
+        flex-direction: column;
+    }
+
+    .app-logo {
+        margin-bottom: 20px;
+    }
+
+    .alter-theme {
+        position: static;
+        margin-bottom: 20px;
+    }
+
+    .container-form-register {
+        width: 100%;
+    }
+}
+
+.pad-10 {
+    padding-top: 10px;
+}
+
+.marg-5 {
+    margin-top: 5px;
 }
 </style>
