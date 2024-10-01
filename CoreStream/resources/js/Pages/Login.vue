338d37b66068a66c5ebdca3bbd60e685cbcc58a4
Pablo <PabloHAlves99@gmail.com>
2024-09-30 21:00:56 -0300

    Update Login.vue

--- CoreStream/resources/js/Pages/Login.vue
+++ CoreStream/resources/js/Pages/Login.vue
@@ -70,7 +70,7 @@ const submit = () => {
                     </div>
 
                     <div class="flex items-center justify-end mt-4">
-                        <Link v-if="canResetPassword" :href="route('password.request')"
+                        <Link
                             class="underline text-sm text-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                         Forgot your password?
                         </Link>
