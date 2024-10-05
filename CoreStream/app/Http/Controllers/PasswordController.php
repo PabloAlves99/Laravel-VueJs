d50a93a1de6bceeb9f0713aa0a4b8e6ee5a75490
Pablo <PabloHAlves99@gmail.com>
2024-10-05 14:39:04 -0300

    up in PasswordController

--- CoreStream/app/Http/Controllers/PasswordController.php
+++ CoreStream/app/Http/Controllers/PasswordController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -13,14 +14,14 @@ class PasswordController extends Controller
     /**
      * Update the user's password.
      */
-    public function update(Request $request): RedirectResponse
+    public function update(Request $request, User $user): RedirectResponse
     {
         $validated = $request->validate([
             'current_password' => ['required', 'current_password'],
             'password' => ['required', Password::defaults(), 'confirmed'],
         ]);
 
-        $request->user()->update([
+        $user->update([
             'password' => Hash::make($validated['password']),
         ]);
 
--- CoreStream/routes/admin.php
+++ CoreStream/routes/admin.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\PasswordController;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\EmailController;
 use App\Http\Controllers\ProfileController;
 
@@ -12,13 +13,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile/{user}', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile/{user}', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile/{user}', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::put('/password/update{user}', [PasswordController::class, 'update'])->name('password.update');
+    Route::put('/email/update/{user}', [EmailController::class, 'update'])->name('email.update');
 });
 
 Route::prefix("admin")->middleware('auth')->group(function () {
     Route::get('/registered', [UserController::class,'registered'])->name('users.registered');
     Route::get('/register', [RegisteredUserController::class, 'create'])->name('users.create.register');
     Route::post('/register', [RegisteredUserController::class, 'store'])->name('users.store.register');
-
-    Route::put('password', [PasswordController::class, 'update'])->name('password.update');
-    Route::put('/email/update/{user}', [EmailController::class, 'update'])->name('email.update');
 });
