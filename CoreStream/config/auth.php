ca97c5879dd990c2b970b7b051f563e6f48082c8
Pablo <PabloHAlves99@gmail.com>
2024-10-07 23:34:10 -0300

    Update auth.php

--- CoreStream/config/auth.php
+++ CoreStream/config/auth.php
@@ -12,6 +12,16 @@ return [
     | as required, but they're a perfect start for most applications.
     |
     */
+    /*
+    |---------------------------------------------------------------- -------------------------
+    | Padrões de autenticação
+    |---------------------------------------------------------------- -------------------------
+    |
+    | Esta opção define o "guard" e a senha de autenticação padrão
+    | redefina o "corretor" para seu aplicativo. Você pode alterar esses valores
+    | conforme necessário, mas são um começo perfeito para a maioria das aplicações.
+    |
+    */
 
     'defaults' => [
         'guard' => env('AUTH_GUARD', 'web'),
@@ -32,6 +42,22 @@ return [
     | system used by the application. Typically, Eloquent is utilized.
     |
     | Supported: "session"
+    |
+    */
+    /*
+    |---------------------------------------------------------------- -------------------------
+    | Protetores de autenticação
+    |---------------------------------------------------------------- -------------------------
+    |
+    | A seguir, você pode definir cada proteção de autenticação para seu aplicativo.
+    | Claro, uma ótima configuração padrão foi definida para você
+    | que utiliza armazenamento de sessão mais o provedor de usuário Eloquent.
+    |
+    | Todos os protetores de autenticação possuem um provedor de usuário, que define como o
+    | os usuários são realmente recuperados do seu banco de dados ou outro armazenamento
+    | sistema utilizado pela aplicação. Normalmente, o Eloquent é utilizado.
+    |
+    | Suportado: "sessão"
     |
     */
 
@@ -56,6 +82,22 @@ return [
     | be assigned to any extra authentication guards you have defined.
     |
     | Supported: "database", "eloquent"
+    |
+    */
+    /*
+    |---------------------------------------------------------------- -------------------------
+    | Provedores de usuários
+    |---------------------------------------------------------------- -------------------------
+    |
+    | Todos os protetores de autenticação possuem um provedor de usuário, que define como o
+    | os usuários são realmente recuperados do seu banco de dados ou outro armazenamento
+    | sistema utilizado pela aplicação. Normalmente, o Eloquent é utilizado.
+    |
+    | Se você tiver várias tabelas ou modelos de usuários, poderá configurar vários
+    | provedores para representar o modelo/tabela. Esses provedores poderão então
+    | ser atribuído a quaisquer guardas de autenticação extras que você definiu.
+    |
+    | Suportado: "banco de dados", "eloquente"
     |
     */
 
@@ -89,6 +131,24 @@ return [
     | quickly generating a very large amount of password reset tokens.
     |
     */
+    /*
+    |---------------------------------------------------------------- -------------------------
+    | Redefinindo senhas
+    |---------------------------------------------------------------- -------------------------
+    |
+    | Estas opções de configuração especificam o comportamento da senha do Laravel
+    | funcionalidade de redefinição, incluindo a tabela utilizada para armazenamento de token
+    | e o provedor de usuário que é invocado para realmente recuperar usuários.
+    |
+    | O tempo de expiração é o número de minutos que cada token de redefinição será
+    | considerado válido. Este recurso de segurança mantém os tokens de curta duração, então
+    | eles têm menos tempo para serem adivinhados. Você pode alterar isso conforme necessário.
+    |
+    | A configuração do acelerador é o número de segundos que um usuário deve esperar antes
+    | gerando mais tokens de redefinição de senha. Isso evita que o usuário
+    | gerando rapidamente uma grande quantidade de tokens de redefinição de senha.
+    |
+    */
 
     'passwords' => [
         'users' => [
@@ -107,6 +167,16 @@ return [
     | Here you may define the amount of seconds before a password confirmation
     | window expires and users are asked to re-enter their password via the
     | confirmation screen. By default, the timeout lasts for three hours.
+    |
+    */
+    /*
+    |---------------------------------------------------------------- -------------------------
+    | Tempo limite de confirmação de senha
+    |---------------------------------------------------------------- -------------------------
+    |
+    | Aqui você pode definir quantos segundos antes da confirmação da senha
+    | janela expira e os usuários são solicitados a digitar novamente sua senha por meio do
+    | tela de confirmação. Por padrão, o tempo limite dura três horas.
     |
     */
 
